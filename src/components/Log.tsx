9ab674b9880f24771067b8773cba5dee390629f4
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-09-03 00:40:54 +0600

    added table functionality

--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -1,44 +1,48 @@
+'use client'
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
-  
-  const invoices = [
-    {
-    date: Date.now(),
-    hour: 5,
-    note: 'test'
-    },
- 
- 
-  ]
-  
-  export function Log() {
-    return (
-      <Table>
-        <TableCaption>List of Logs</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Date</TableHead>
-            <TableHead>Hour</TableHead>
-            <TableHead className="text-right">Note</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {invoices.map((invoice,index) => (
-            <TableRow key={index}>
-              <TableCell className="font-medium">{invoice.date}</TableCell>
-              <TableCell>{invoice.hour}</TableCell>
-              <TableCell className="text-right">{invoice.note}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    )
-  }
-  
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { useLogStore } from "@/store"
+
+
+
+export function Log() {
+
+  const logs = useLogStore(state => state.logs);
+
+
+  return (
+    <Table>
+      <TableCaption>List of Logs</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Date</TableHead>
+          <TableHead>Hour</TableHead>
+          <TableHead className="text-right">Note</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+
+        {
+          Object.keys(logs).map((key) => {
+            const log = logs[key];
+
+            return (<TableRow key={key}>
+              <TableCell className="font-medium">{log.date.toDateString()}</TableCell>
+              <TableCell>{log.hour}</TableCell>
+              <TableCell className="text-right">{log.note}</TableCell>
+            </TableRow>)
+          })
+        }
+
+
+      </TableBody>
+    </Table>
+  )
+}
